a4d9c1906d63ed4bebd08cb07ce92a895d304be4
Brita <britapuusemp@gmail.com>
2025-03-07 09:11:07 +0200

    JANNE recovery

--- src/main/java/ee/avastaeesti/gameback/controller/game/GameController.java
+++ src/main/java/ee/avastaeesti/gameback/controller/game/GameController.java
@@ -1,7 +1,7 @@
 package ee.avastaeesti.gameback.controller.game;
 
 import ee.avastaeesti.gameback.controller.location.dto.LocationsInfo;
-import ee.avastaeesti.gameback.service.game.NewGameService;
+import ee.avastaeesti.gameback.service.game.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +10,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class NewGameController {
+public class GameController {
 
-    private final NewGameService newGameService;
+    private final GameService gameService;
 
     @GetMapping("/locations")
     public List<LocationsInfo> getLocations() {
-        List<LocationsInfo> locations = newGameService.getLocations();
+        List<LocationsInfo> locations = gameService.getLocations();
         return locations;
 
     }
--- src/main/java/ee/avastaeesti/gameback/service/game/GameService.java
+++ src/main/java/ee/avastaeesti/gameback/service/game/GameService.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class NewGameService {
+public class GameService {
 
     private final LocationRepository locationRepository;
     private final LocationInfoMapper locationInfoMapper;
