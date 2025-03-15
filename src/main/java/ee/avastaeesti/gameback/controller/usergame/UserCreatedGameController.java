4cb27324c6083fa61cf4c709cacbf211259766fc
Brita <britapuusemp@gmail.com>
2025-03-15 18:29:54 +0200

    Brita - GameScore get teenus

--- src/main/java/ee/avastaeesti/gameback/controller/usergame/UserCreatedGameController.java
+++ src/main/java/ee/avastaeesti/gameback/controller/usergame/UserCreatedGameController.java
@@ -1,12 +1,12 @@
 package ee.avastaeesti.gameback.controller.usergame;
 
+import ee.avastaeesti.gameback.controller.randomgame.dto.GameOverResults;
 import ee.avastaeesti.gameback.controller.randomgame.dto.NextRandomLocation;
+import ee.avastaeesti.gameback.controller.randomgame.dto.RandomLocationAnswerResult;
+import ee.avastaeesti.gameback.controller.randomgame.dto.UserAnswer;
 import ee.avastaeesti.gameback.service.usergame.UserCreatedGameService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +25,19 @@ public class UserCreatedGameController {
         Integer newUserGame = userCreatedGameService.createNewUserGame(gameId, userId);
         return newUserGame;
     }
+
+    @PostMapping("/user/game/location/result")
+    public RandomLocationAnswerResult getLocationResult(@RequestBody UserAnswer userAnswer) {
+        return userCreatedGameService.getLocationResult(userAnswer);
+    }
+
+    @GetMapping("/user/game/gameover")
+    public GameOverResults getGameOverResults(@RequestParam Integer userGameId) {
+        GameOverResults gameOverResults = userCreatedGameService.getGameOverResults(userGameId);
+        return gameOverResults;
+    }
+
+    public getGameScoreResults(@RequestParam Integer )
 
 
 }
--- src/main/java/ee/avastaeesti/gameback/persistence/usergamelocation/UserGameLocationRepository.java
+++ src/main/java/ee/avastaeesti/gameback/persistence/usergamelocation/UserGameLocationRepository.java
@@ -1,9 +1,11 @@
 package ee.avastaeesti.gameback.persistence.usergamelocation;
 
+import ee.avastaeesti.gameback.persistence.randomgamelocation.RandomGameLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserGameLocationRepository extends JpaRepository<UserGameLocation, Integer> {
@@ -11,6 +13,12 @@ public interface UserGameLocationRepository extends JpaRepository<UserGameLocati
 
     @Query("select u from UserGameLocation u where u.userGame.id = :userGameId and u.state = :state")
     Optional<UserGameLocation> findNextLocationBy(Integer userGameId, String state);
+
+    @Query("select u from UserGameLocation u where u.userGame.id = :userGameId and u.location.id = :locationId")
+    Optional<UserGameLocation> findAnsweredLocationBy(Integer userGameId, Integer locationId);
+
+    @Query("select u from UserGameLocation u where u.userGame.id = :userGameId")
+    List<UserGameLocation> findGameBy(Integer userGameId);
 
 
 }
